c22b42e781128a97d3f69ffe89a9cb3a823baa58
xGeorgy <81165927+xGeorgy@users.noreply.github.com>
2021-06-06 18:55:25 +0600

    Add files via upload

---
+++ first.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y;
+sudo apt install python3-pip -y;
+sudo pip3 install youtube-dl -y;
+sudo apt install ffmpeg -y;
+touch /sbin/mp3-get;
+#chmod 755 /sbin/mp3-get;
---
+++ second.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+chmod 755 mp3-get;
+sudo cp mp3-get /sbin/mp3-get;
