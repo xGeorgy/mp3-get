cf8033f50287e348abd537a1e65c5f1c09940118
xGeorgy <81165927+xGeorgy@users.noreply.github.com>
2021-06-06 18:58:51 +0600

    Add files via upload

--- first.sh
+++ first.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y;
-sudo apt install python3-pip -y;
+sudo apt install python3-pip;
 sudo pip3 install youtube-dl -y;
 sudo apt install ffmpeg -y;
 touch /sbin/mp3-get;
